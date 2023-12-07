e488fc185803f2c8e4b9d35b6c723e9dd7f0a2ad
Mason Bybee <bybeemason@gmail.com>
2023-12-07 13:05:35 -0600

    added LoginUserForm

--- forms.py
+++ forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, ValidationError
 
 
 class NewUserForm(FlaskForm):
-    username = StringField("Username", validators=[InputRequired()])
+    def validate_name(form, field):
+        if len(field.data) > 20:
+            raise ValidationError("Username must be less than 20 characters")
+
+    username = StringField("Username", validators=[InputRequired(), validate_name])
     email = EmailField("Email", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[InputRequired()])
+
+
+class LoginUserForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
